06f346cbf08ae8fbc8c20ceb865f9c5cbaced201
Mr.Snippy <phonarnun@hotmail.com>
2021-05-09 17:12:32 +0700

    Remove Keystroke.js and remove unnecessary words in the app.js

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ var avg_UD_pass = 0;
 var avg_DU_pass = 0;
 
 var con = mysql.createConnection({
-<<<<<<< HEAD
   host: "localhost",
   user: "root",
   database: 'ccs'
@@ -45,41 +44,6 @@ router.get('/Login_Successful',function (req, res){
 
 app.post('/register_data', (req, res) => {
   console.log('body: ', JSON.stringify(req.body));
-=======
-    host: "localhost",
-    user: "root",
-    database: 'ccs'
-});
-
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    });
-
-  app.use('/',router);
-
-  router.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/mainpage.html'));
-});
-
-function rangefinder(avgdata){
-  var range = (avgdata * 75) / 100;
-  return range;
-}
-app.post('/',(req,res) => {
-  var username = req.body.form.kuser;
-  var password = req.body.kpass;
-  console.log(username);
-  var sql = 'SELECT * FROM LoginInfo';
-  con.query(sql,(err,data) => {
-    if(err) throw err;
-    console.log(data);
-  })
-})
-
-app.post('/data',function(req, res){
-  console.log('body: ',  JSON.stringify(req.body));
->>>>>>> master
   res.send(req.body);
   avg_CPM_user = parseFloat(JSON.stringify(req.body.avg_CPM_user));
   avg_UD_user = parseFloat(JSON.stringify(req.body.avg_UD_user));
@@ -89,7 +53,6 @@ app.post('/data',function(req, res){
   avg_DU_pass = parseFloat(JSON.stringify(req.body.avg_DU_pass));
   var username = req.body.username;
   var password = req.body.password;
-<<<<<<< HEAD
   bcrypt.hash(password, 10, function (err, hash) {
     var sql = 'INSERT INTO logininfo VALUES (\'' + username + '\',\'' + hash + '\',' + avg_DU_user + ',' + avg_UD_user + ',' + avg_CPM_user + ',' + avg_DU_pass + ',' + avg_UD_pass + ',' + avg_CPM_pass + ')';
     console.log(hash);
@@ -182,62 +145,5 @@ app.post('/', function (req, res) {
 })
 
 app.listen(8081, function () {
-=======
-  var test = "test";
-  console.log(test);
-  var sql = 'SELECT * FROM LoginInfo where username=\'' + username + '\'';
-  console.log(username);
-  con.query(sql,(err,data) => {
-    if(err) throw err;
-    console.log(password)
-    console.log(data[0].password)
-      if(data[0].password == password){
-        console.log("Password pass")
-        if ((data[0].avgCPM_user + rangefinder(data[0].avgCPM_user) >= avg_CPM_user && (data[0].avgCPM_user - rangefinder(data[0].avgCPM_user) <= avg_CPM_user))) {
-          if ((data[0].avgUD_user + rangefinder(data[0].avgUD_user) >= avg_UD_user && (data[0].avgUD_user - rangefinder(data[0].avgUD_user) <= avg_UD_user))) {
-            if ((data[0].avgDU_user + rangefinder(data[0].avgDU_user) >= avg_DU_user && (data[0].avgDU_user - rangefinder(data[0].avgDU_user) <= avg_DU_user))) {
-              if ((data[0].avgCPM_pass + rangefinder(data[0].avgCPM_pass) >= avg_CPM_pass && (data[0].avgCPM_pass - rangefinder(data[0].avgCPM_pass) <= avg_CPM_pass))) {
-                if ((data[0].avgUD_pass + rangefinder(data[0].avgUD_pass) >= avg_UD_pass && (data[0].avgUD_pass - rangefinder(data[0].avgUD_pass) <= avg_UD_pass))) {
-                  if ((data[0].avgDU_pass + rangefinder(data[0].avgDU_pass) >= avg_DU_pass && (data[0].avgDU_pass - rangefinder(data[0].avgDU_pass) <= avg_DU_pass))) {
-                      console.log('Login Successful')
-                  }
-                  else
-                  {
-                    console.log('DU of password time not match');
-                  }
-                }
-                else
-                {
-                  console.log('UD of password time not match');
-                }
-              }
-              else
-              {
-                console.log('CPM of password time not match');
-              }
-            }
-            else
-            {
-              console.log('DU of username time not match');
-            }
-          }
-          else {
-            console.log('UD of username time not match');
-          }
-        }
-        else {
-          console.log('CPM of username time not match');
-        }
-      }
-      else
-      {
-        console.log('Wrong password');
-      }
-  })
-})
-
-
-app.listen(8081, function(){
->>>>>>> master
   console.log('Listening at Port ' + 8081);
 })
